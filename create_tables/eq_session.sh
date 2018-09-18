474f64e120e86f3709b4a7550f80f2129d816221
Mebin Abraham <mebin.abraham@qa.com>
2018-09-18 10:24:53 +0100

    Test

--- create_tables/eq_session.sh
+++ create_tables/eq_session.sh
@@ -5,10 +5,11 @@ aws dynamodb --endpoint http://localhost:8000/ create-table \
 	--table-name dev-eq-session \
     --attribute-definitions AttributeName=eq_session_id,AttributeType=S \
 	--key-schema AttributeName=eq_session_id,KeyType=HASH \
-	--provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+	--provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+	--time-to-live-specification "Enabled=true, AttributeName=expires_at"
 
-aws dynamodb update-time-to-live \
-    --table-name dev-eq-session \
-    --time-to-live-specification "Enabled=true, AttributeName=expires_at"
+#aws dynamodb update-time-to-live \
+#    --table-name dev-eq-session \
+#    --time-to-live-specification "Enabled=true, AttributeName=expires_at"
 
 aws dynamodb --endpoint http://localhost:8000/ list-tables
